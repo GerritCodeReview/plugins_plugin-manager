f646d3af3748f51ab9f7e56edfb66e86af2c3c24
Luca Milanesio <luca.milanesio@gmail.com>
2015-11-08 06:58:36 -0800

    Allows navigation from Gerrit top-menu to manager
    Adds a entry Plugins>Manage to Gerrit Top-Menu if:
    - remote plugin management is enabled
    - current user has admin permissions
    
    The list of available plugins includes both version + sha1
    allowing to match them against the list of loaded plugins.
    Plugins that are already installed and loaded are not shown anymore
    in the list of the ones available.
    
    Change-Id: I8c7f6841577a16698249f84d8a6924f8ebfddfe1

--- src/main/resources/static/js/plugin-manager.js
+++ src/main/resources/static/js/plugin-manager.js
@@ -14,24 +14,49 @@
 
 var app = angular.module('PluginManager', []).controller(
     'LoadInstalledPlugins',
-    function($http) {
+    function($scope, $http) {
       var plugins = this;
 
       plugins.list = {};
 
       plugins.available = {};
 
-      $http.get('/plugins/?all', plugins.httpConfig).then(
-          function successCallback(response) {
-            plugins.list = response.data;
-          }, function errorCallback(response) {
-          });
+      $scope.refreshInstalled = function(hideAvailable) {
+        $http.get('/plugins/?all', plugins.httpConfig).then(
+            function successCallback(response) {
+              plugins.list = response.data;
+              if(hideAvailable) {
+                $.each( plugins.list, function(id, props) {
+                  var escapedVersion = props.version.replace(/\./g, "\\.");
+                  $("tr#" + id + "-" + escapedVersion).addClass("hidden");
+                });
+              }
+            }, function errorCallback(response) {
+            });
+      }
+
+      $scope.install = function(id, url) {
+        var pluginInstallData = { "url" : url };
+        $("button#" + id).addClass("hidden");
+        $("span#installing-" + id).removeClass("hidden");
+        $http.put('/plugins/' + id + ".jar",
+            pluginInstallData).then(
+                function successCallback(response) {
+                  $("span#installing-" + id).addClass("hidden");
+                  $("span#installed-" + id).removeClass("hidden");
+                  $scope.refreshInstalled(false);
+            }, function errorCallback(response) {
+              $("span#installing-" + id).addClass("hidden");
+              $("span#failed-" + id).removeClass("hidden");
+            });
+      }
       
       $http.get('/plugins/plugin-manager/available', plugins.httpConfig).then(
           function successCallback(response) {
             plugins.available = response.data;
+            $scope.refreshInstalled(true);
           }, function errorCallback(response) {
-          });
+          });      
     });
 
 app.config(function($httpProvider) {
