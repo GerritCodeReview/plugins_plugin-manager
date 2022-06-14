99428eefc225bbbb3935c7562be5c357114aafe9
David Ostrovsky <david@ostrovsky.org>
2022-06-14 20:46:44 +0200

    Bump servlet API to 5.0
    Change-Id: Iae7e94365b0e672f29605c4f01b8a69fb2940a86
    Depends-On: https://gerrit-review.googlesource.com/c/gerrit/+/327195

--- src/main/java/com/googlesource/gerrit/plugins/manager/XAuthFilter.java
+++ src/main/java/com/googlesource/gerrit/plugins/manager/XAuthFilter.java
@@ -22,17 +22,17 @@ import com.google.gerrit.httpd.WebSession;
 import com.google.gerrit.server.AccessPath;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponseWrapper;
 import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServletResponseWrapper;
 
 @Singleton
 public class XAuthFilter implements Filter {
